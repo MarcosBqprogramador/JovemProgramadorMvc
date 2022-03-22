198d89d42d94c4378e75c92d474a4c58813c0b80
Marcos Paulo <marcospaulodesouza1502@gmail.com>
2022-03-21 21:45:10 -0300

    Adicionar arquivos de projeto.

---
+++ JovemProgramadorMvc/Controllers/AlunoController.cs
@@ -0,0 +1,66 @@
+﻿using JovemProgramadorMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace JovemProgramadorMvc.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public AlunoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+        public IActionResult Mensagem()
+        {
+            return View();
+        }
+        public async Task<IActionResult> BuscarCep(string cep)
+        {
+            EnderecoModel enderecoModel = new();
+            try
+            {
+                cep = cep.Replace("-", "");
+                using var client = new HttpClient();
+                var result = await client.GetAsync(_configuration.GetSection("ApiCep")["BaseUrl"] + cep + "/json");
+                if (result.IsSuccessStatusCode)
+                {
+                    enderecoModel = JsonSerializer.Deserialize<EnderecoModel>(
+                        await result.Content.ReadAsStringAsync(), new JsonSerializerOptions() { });
+                    if (enderecoModel.complemento == "")
+                    {
+                        enderecoModel.complemento = "Sem complemento";
+                    }
+                    if (Regex.IsMatch(cep, (@"000")) == true)
+                    {
+                        enderecoModel.logradouro = "CEP geral de " + enderecoModel.localidade;
+                        enderecoModel.bairro = "Não especificado";
+                    }
+                }
+                else
+                {
+                    ViewData["Mensagem"] = "Erro na busca do endereço!";
+                    return View("Index");
+                }
+            }
+            catch (Exception e)
+            {
+            }
+            return View("Buscarcep", enderecoModel);
+        }
+    }
+}
